fc84741488f04a36f02a7c8874ae2c517199ed56
saymanq <87442765+saymanq@users.noreply.github.com>
2024-12-01 01:11:05 -0700

    Update: Added support for file path for API

--- src/index.ts
+++ src/index.ts
@@ -18,18 +18,26 @@ app.get('study/health', (c) => {
   return c.json({ status: 'healthy', code: 200 })
 })
 
-app.get('study/extract/:id', async (c) => {
-  const id = c.req.param('id')
-  const bucket: R2Bucket = c.env.MY_BUCKET
-  const object = await bucket.get(id)
-  if (!object) {
-    return c.text('File not found', 404)
+app.get('study/extract/:userId/:filename', async (c) => {
+  try {
+    const userId = c.req.param('userId')
+    const filename = c.req.param('filename')
+    const id = `${userId}/${filename}`
+    console.log("Worker getting executed !!")
+    const bucket: R2Bucket = c.env.MY_BUCKET
+    const object = await bucket.get(id)
+    if (!object) {
+      return c.json({ error: 'File not found' }, 500)
+    }
+    const data = await object.arrayBuffer()
+    const apiKey = c.env.LLAMA_CLOUD_API_KEY;
+    const reader = new LlamaParseReader({ resultType: "markdown", apiKey });
+    const documents = await reader.loadDataAsContent(new Uint8Array(data as ArrayBuffer));
+    return c.json(documents)
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+    return c.json({ error: errorMessage }, 500)
   }
-  const data = await object.arrayBuffer()
-  const apiKey = c.env.LLAMA_CLOUD_API_KEY;
-  const reader = new LlamaParseReader({ resultType: "markdown", apiKey });
-  const documents = await reader.loadDataAsContent(new Uint8Array(data as ArrayBuffer));
-  return c.json(documents)
 })
 
 
